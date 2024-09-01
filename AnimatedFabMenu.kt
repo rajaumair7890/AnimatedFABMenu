e4d818e87c5c44e21eb7c774a3e46de443f33542
Kashif Mehmood <kashismails@gmail.com>
2024-09-02 02:31:06 +0500

    Update AnimatedFabMenu.kt
    simplified animation code

--- AnimatedFabMenu.kt
+++ AnimatedFabMenu.kt
@@ -2,18 +2,12 @@ package com.codingWithUmair.AnimatedFabMenu
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
-import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.core.updateTransition
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.animation.scaleIn
-import androidx.compose.animation.scaleOut
-import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -47,187 +41,121 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.fastForEach
+import com.kashif.myapplication.R
 
 @Composable
 fun AnimatedFabMenu(
-	@DrawableRes icon: Int,
-	@StringRes text: Int,
-	items: List<FabItem>,
-	onItemClick: (FabItem) -> Unit,
-	fabColor: Color = MaterialTheme.colorScheme.tertiary,
-	menuColor: Color = MaterialTheme.colorScheme.surfaceVariant,
-	expandedWidth: Dp = 140.dp,
-){
+    @DrawableRes icon: Int,
+    @StringRes text: Int,
+    items: List<FabItem>,
+    onItemClick: (FabItem) -> Unit,
+    fabColor: Color = MaterialTheme.colorScheme.tertiary,
+    menuColor: Color = MaterialTheme.colorScheme.surfaceVariant,
+    expandedWidth: Dp = 180.dp,
+) {
+    var isFabExpanded by remember { mutableStateOf(false) }
 
-	var isFabExpanded by remember {
-		mutableStateOf(false)
-	}
+    val dpAnimationSpec = tween<Dp>(300, easing = LinearEasing)
+    val floatAnimationSpec = tween<Float>(300, easing = LinearEasing)
 
-	val fabTransition = updateTransition(targetState = isFabExpanded, label = "fabTransition")
+    val fabSize by animateDpAsState(
+        targetValue = if (isFabExpanded) expandedWidth else 75.dp,
+        animationSpec = dpAnimationSpec, label = ""
+    )
 
-	val expandFabHorizontally by fabTransition.animateDp(
-		transitionSpec = {
-			tween(300, if(isFabExpanded) 0 else 50, LinearEasing)
-		},
-		label = "expandVertically",
-		targetValueByState = {isExpanded ->
-			if (isExpanded) expandedWidth else 75.dp
-		}
-	)
+    val fabHeight by animateDpAsState(
+        targetValue = if (isFabExpanded) 60.dp else 75.dp,
+        animationSpec = dpAnimationSpec, label = ""
+    )
 
-	val shrinkFabVertically by fabTransition.animateDp(
-		transitionSpec = {
-			tween(300, if(isFabExpanded) 0 else 50, LinearEasing)
-		},
-		label = "shrinkHorizontally",
-		targetValueByState = {isExpanded ->
-			if (isExpanded) 60.dp else 75.dp
-		}
-	)
+    val menuHeight by animateDpAsState(
+        targetValue = if (isFabExpanded) (items.size * 60).dp else 0.dp,
+        animationSpec = dpAnimationSpec, label = ""
+    )
 
-	val menuHeight by fabTransition.animateDp(
-		transitionSpec = {
-			tween(300, if(isFabExpanded) 0 else 50, LinearEasing)
-		},
-		label = "menuHeight",
-		targetValueByState = {isExpanded ->
-			if (isExpanded) (items.size * 50).dp else 0.dp
-		}
-	)
+    ElevatedCard(
+        modifier = Modifier.padding(4.dp),
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.elevatedCardElevation(4.dp),
+        colors = CardDefaults.elevatedCardColors().copy(containerColor = menuColor)
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.height(menuHeight)
+        ) {
+            items.forEach { item ->
+                AnimatedVisibility(
+                    visible = isFabExpanded,
+                    enter = fadeIn(floatAnimationSpec),
+                    exit = fadeOut(floatAnimationSpec)
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .width(fabSize)
+                            .height(60.dp)
+                            .clickable { onItemClick(item) },
+                        horizontalArrangement = Arrangement.Start,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            painter = painterResource(id = item.icon),
+                            contentDescription = null,
+                            modifier = Modifier.size(40.dp),
+                            tint = MaterialTheme.colorScheme.primary
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(
+                            text = stringResource(id = item.text),
+                            modifier = Modifier.padding(12.dp),
+                            fontWeight = FontWeight.Bold,
+                            style = MaterialTheme.typography.labelSmall,
+                            color = MaterialTheme.colorScheme.primary
+                        )
+                    }
+                }
+            }
+        }
 
-	ElevatedCard(
-		modifier = Modifier.padding(4.dp),
-		shape = RoundedCornerShape(16.dp),
-		elevation = CardDefaults.elevatedCardElevation(4.dp),
-		colors = CardDefaults.elevatedCardColors().copy(
-			containerColor = menuColor
-		)
-	) {
-		Column(
-			horizontalAlignment = Alignment.CenterHorizontally,
-			verticalArrangement = Arrangement.SpaceEvenly,
-			modifier = Modifier
-				.height(menuHeight)
-		) {
-			items.forEach { item ->
-				AnimatedVisibility(
-					visible = isFabExpanded,
-					label = "${item.text}",
-					enter = fadeIn(tween(300, 150, LinearEasing)),
-					exit = fadeOut(tween(200, 50, LinearEasing)) + scaleOut(),
-				) {
-					Row(
-						modifier = Modifier
-							.width(expandFabHorizontally)
-							.clickable { onItemClick(item) },
-						horizontalArrangement = Arrangement.Center,
-						verticalAlignment = Alignment.CenterVertically
-					) {
-						Icon(
-							painterResource(id = item.icon),
-							contentDescription = null,
-							modifier = Modifier.padding(start = 16.dp, top = 12.dp, bottom = 12.dp),
-							tint = MaterialTheme.colorScheme.primary
-						)
-						Spacer(modifier = Modifier.weight(1f))
-						Text(
-							text = stringResource(id = item.text),
-							modifier = Modifier.padding(end = 16.dp, top = 12.dp, bottom = 12.dp),
-							fontWeight = FontWeight.Bold,
-							style = MaterialTheme.typography.bodyLarge,
-							color = MaterialTheme.colorScheme.primary
-						)
-					}
-				}
-			}
-		}
-
-		FloatingActionButton(
-			elevation = FloatingActionButtonDefaults.elevation(4.dp),
-			shape = RoundedCornerShape(16.dp),
-			containerColor = fabColor,
-			onClick = { isFabExpanded = !isFabExpanded },
-			modifier = Modifier
-				.width(expandFabHorizontally)
-				.height(shrinkFabVertically)
-		) {
-			AnimatedContent(
-				isFabExpanded,
-				transitionSpec = {
-					when(targetState){
-						true -> {
-							(
-								scaleIn(
-									tween(100, 300, FastOutSlowInEasing)
-								) + fadeIn(
-									tween(250, 300, LinearEasing)
-								)
-							) .togetherWith(
-								fadeOut(
-									tween(10, 0, LinearEasing)
-								)
-							)
-						}
-						false -> {
-							fadeIn(
-								tween(250, 200, FastOutSlowInEasing)
-							).togetherWith(
-								fadeOut(
-									tween(10, 0, LinearEasing),
-								)
-							)
-						}
-					}
-				},
-				label = "FabContent"
-			){ isExpanded ->
-				when(isExpanded){
-					true -> {
-						Row(
-							verticalAlignment = Alignment.CenterVertically,
-							horizontalArrangement = Arrangement.Center,
-						) {
-							Spacer(modifier = Modifier.size(16.dp))
-							Icon(
-								painter = painterResource(id = icon),
-								contentDescription = null,
-								modifier = Modifier
-									.size(40.dp)
-							)
-							Spacer(modifier = Modifier.weight(1f))
-							Text(
-								text = stringResource(id = text),
-								modifier = Modifier.padding(end = 16.dp),
-								fontWeight = FontWeight.Bold,
-								style = MaterialTheme.typography.headlineSmall
-							)
-						}
-					}
-
-					false -> {
-						Icon(
-							painter = painterResource(id = icon),
-							contentDescription = null,
-							modifier = Modifier
-								.size(40.dp)
-						)
-					}
-				}
-			}
-		}
-	}
-
+        FloatingActionButton(
+            elevation = FloatingActionButtonDefaults.elevation(4.dp),
+            shape = RoundedCornerShape(16.dp),
+            containerColor = fabColor,
+            onClick = { isFabExpanded = !isFabExpanded },
+            modifier = Modifier
+                .size(fabSize, fabHeight)
+        ) {
+            Icon(
+                painter = painterResource(id = icon),
+                contentDescription = null,
+                modifier = Modifier.size(40.dp)
+            )
+        }
+    }
 }
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun FabPreview() {
+    val items = listOf(
+        FabItem(icon = R.drawable.ic_launcher_foreground, text = R.string.app_name),
+        FabItem(icon = R.drawable.ic_launcher_foreground, text = R.string.app_name),
+        FabItem(icon = R.drawable.ic_launcher_foreground, text = R.string.app_name),
+        FabItem(icon = R.drawable.ic_launcher_foreground, text = R.string.app_name),
+        FabItem(icon = R.drawable.ic_launcher_foreground, text = R.string.app_name),
+        FabItem(icon = R.drawable.ic_launcher_foreground, text = R.string.app_name)
+    )
 
-// @Preview(showBackground = true, showSystemUi = true)
-// @Composable
-// fun FabPreview(){
-// 	Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
-// 		AnimatedFabMenu()
-// 	}
-// }
-
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        AnimatedFabMenu(
+            icon = R.drawable.ic_launcher_foreground,
+            text = R.string.app_name,
+            items = items,
+            onItemClick = { }
+        )
+    }
+}
 data class FabItem(
-	@DrawableRes val icon: Int,
-	@StringRes val text: Int
+    @DrawableRes val icon: Int,
+    @StringRes val text: Int
 )
